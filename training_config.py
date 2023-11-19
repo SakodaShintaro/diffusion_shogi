d843214880cb825ecc5676b6dd26067a71f20286
Shintaro Sakoda <sakoda@keio.jp>
2023-11-19 16:32:20 +0900

    Fixed num_epochs to 5
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- training_config.py
+++ training_config.py
@@ -7,7 +7,7 @@ class TrainingConfig:
     """Training configuration."""
 
     train_batch_size = 1024
-    num_epochs = 10
+    num_epochs = 5
     gradient_accumulation_steps = 1
     learning_rate = 1e-4
     lr_warmup_steps = 500
