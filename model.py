103506c9919a831cc117d627576d037774669655
Shintaro Sakoda <sakoda@keio.jp>
2023-11-19 09:48:44 +0900

    Add time encoding to TransformerModel
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 import torch
 import torch.jit
 import torch.nn as nn
@@ -15,6 +16,31 @@ CHANNEL_NUM_TO_NHEAD = {
 SQUARE_NUM = BOARD_SIZE ** 2
 
 
+class IntEncoding(nn.Module):
+    """
+    Reference PositionalEncoding in https://pytorch.org/tutorials/beginner/transformer_tutorial.html
+    """
+
+    def __init__(self, d_model: int, max_value: int) -> None:
+        super().__init__()
+        position = torch.arange(max_value).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2)
+                             * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_value, 1, d_model)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Arguments:
+            t: Tensor, shape ``[batch_size,]``
+        Return:
+            embedded, shape``[batch_size, 1, dim]``
+        """
+        return self.pe[t]  # type: ignore
+
+
 class TransformerModel(nn.Module):
     def __init__(self, input_channel_num: int, block_num: int, channel_num: int) -> None:
         super(TransformerModel, self).__init__()
@@ -23,6 +49,7 @@ class TransformerModel(nn.Module):
         self.first_encoding_ = torch.nn.Linear(input_channel_num, channel_num)
         self.positional_encoding_ = torch.nn.Parameter(
             torch.zeros([SQUARE_NUM, 1, channel_num]), requires_grad=True)
+        self.time_encoding_ = IntEncoding(channel_num, 1000)
         encoder_layer = torch.nn.TransformerEncoderLayer(
             channel_num,
             nhead=nhead,
@@ -38,11 +65,16 @@ class TransformerModel(nn.Module):
         batch_size = x.shape[0]
         channel_num = x.shape[1]
         board_size = x.shape[2]
+        te = self.time_encoding_(t)
+        te = te.squeeze(1)
+        te = te.unsqueeze(0)
         x = x.view([batch_size, channel_num, board_size * board_size])
         x = x.permute([2, 0, 1])  # [bs, ch, H*W] -> [H*W, bs, ch]
         x = self.first_encoding_(x)
         x = x + self.positional_encoding_
+        x = torch.cat([x, te], dim=0)
         x = self.encoder_(x)
+        x = x[0:-1]
         x = self.head_(x)
         x = x.permute([1, 2, 0])  # [H*W, bs, ch] -> [bs, ch, H*W]
         x = x.view([batch_size, channel_num, board_size, board_size])
